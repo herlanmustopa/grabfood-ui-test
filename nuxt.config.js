00bcb4186f5572a52f3e018b7d98b234700d537b
herlanmustopa <hrmustopa.dev@gmail.com>
2022-01-08 21:16:12 +0700

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
+  distDir: "build",
+  target: "serverless",
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
@@ -65,4 +67,7 @@ export default {
   // },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  // generate: {
+  //   dir: "my-site",
+  // },
 };
